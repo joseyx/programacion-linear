7a83495fa74634a69b54d091a7f8101a9327b042
Jose Rojas <joseyx041199@gmail.com>
2023-09-24 20:53:20 -0400

    Added tailwind styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
 	const [optimize, setOptimize] = useState("");
 	const [results, setResults] = useState(null);
 
-	// Función para agregar una restricción
 	const handleAddConstraint = () => {
 		const constraintName = prompt(
 			"Ingrese el nombre de la variable a restringir:"
@@ -19,9 +18,8 @@ function App() {
 		}));
 	};
 
-	// Función para agregar una variable
 	const handleAddVariable = () => {
-		const variableName = prompt("Ingrese el nombre de la variable:");
+		const variableName = prompt("Ingrese el nombre del producto:");
 		const variableValues = {};
 		const variableValueCount = prompt(
 			"Ingrese la cantidad de valores que tendrá la variable:"
@@ -41,7 +39,6 @@ function App() {
 		}));
 	};
 
-	// Función para resolver el problema de optimización
 	const handleSolve = () => {
 		const model = {
 			optimize,
@@ -55,33 +52,62 @@ function App() {
 	};
 
 	return (
-		<div>
-			<h1>Problema de minimización. Programación Lineal</h1>
+		<div className="container mx-auto p-4">
+			<h1 className="text-2xl font-bold mb-4">
+				Problema de minimización. Programación Lineal
+			</h1>
 
-			<h2>Variables</h2>
-			<button onClick={handleAddVariable}>Agregar variable</button>
-			<pre>{JSON.stringify(variables, null, 2)}</pre>
+			<div className="mb-4">
+				<h2 className="text-lg font-bold mb-2">Variables</h2>
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+					onClick={handleAddVariable}
+				>
+					Agregar variable
+				</button>
+				<pre className="mt-2 p-2 bg-gray-200">
+					{JSON.stringify(variables, null, 2)}
+				</pre>
+			</div>
 
-			<h2>Restricciones</h2>
-			<button onClick={handleAddConstraint}>Agregar restricción</button>
-			<pre>{JSON.stringify(constraints, null, 2)}</pre>
+			<div className="mb-4">
+				<h2 className="text-lg font-bold mb-2">Restricciones</h2>
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+					onClick={handleAddConstraint}
+				>
+					Agregar restricción
+				</button>
+				<pre className="mt-2 p-2 bg-gray-200">
+					{JSON.stringify(constraints, null, 2)}
+				</pre>
+			</div>
 
-			<h2>Optimización</h2>
-			<label>
-				Variable a optimizar:
-				<input
-					type="text"
-					value={optimize}
-					onChange={(e) => setOptimize(e.target.value)}
-				/>
-			</label>
-
-			<button onClick={handleSolve}>Mostrar solución</button>
+			<div className="mb-4">
+				<h2 className="text-lg font-bold mb-2">Optimización</h2>
+				<label className="block mb-2">
+					Variable a optimizar:
+					<input
+						className="border border-gray-400 rounded px-2 py-1"
+						type="text"
+						value={optimize}
+						onChange={(e) => setOptimize(e.target.value)}
+					/>
+				</label>
+				<button
+					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+					onClick={handleSolve}
+				>
+					Mostrar solución
+				</button>
+			</div>
 
 			{results && (
 				<div>
-					<h2>Resultados</h2>
-					<pre>{JSON.stringify(results, null, 2)}</pre>
+					<h2 className="text-lg font-bold mb-2">Resultados</h2>
+					<pre className="p-2 bg-gray-200">
+						{JSON.stringify(results, null, 2)}
+					</pre>
 				</div>
 			)}
 		</div>
